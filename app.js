0d0b57cab16d4cff6777129e4237728cc09d90bf
niksankarkee <karkeeniksan@gmail.com>
2020-06-10 21:32:17 +0900

    tillParsingRequest

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const routes = require('./routes');
-console.log(routes.someText);
+const app = express();
 
-const server = http.createServer(routes.handler);
-server.listen(3000, () => {
-    console.log('listening');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+})
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 })
 
+app.use('/', (req, res, next) => {
+    res.send('<h1>Hello from express!</h1>');
+})
+
+app.listen(3000);
+
+//  Parsing request
