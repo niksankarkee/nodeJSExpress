0681df082a71f37489dd303766680438c2712d68
niksankarkee <karkeeniksan@gmail.com>
2020-06-11 21:44:59 +0900

    Before dynamic Content

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 
 const app = express();
@@ -10,16 +12,18 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRoute);
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/admin', adminRoute);
 
 app.use(shopRoute);
 
 app.use('/', (req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 
 })
 
 app.listen(3000);
 
-// Filtering Paths
+
 
