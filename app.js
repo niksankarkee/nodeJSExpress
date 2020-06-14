a520b6373e36e38cfa4689f1b577ecfbe44257e5
niksankarkee <karkeeniksan@gmail.com>
2020-06-14 18:34:27 +0900

    ejs

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
 const path = require('path');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoute = require('./routes/admin');
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+
+const adminData = require('./routes/admin');
 
 const shopRoute = require('./routes/shop');
 
-const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin', adminRoute);
+app.use('/admin', adminData.routes);
 
 app.use(shopRoute);
 
-app.use('/', (req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+app.use((req, res, next) => {
+    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', { pageTitle: 'Page Not Found' })
 
 })
 
--- routes/admin.js
+++ routes/admin.js
@@ -6,17 +6,26 @@ const rootDir = require('../util/path');
 
 const router = express.Router();
 
+const products = [];
+
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    // res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
+    res.render('add-product', {
+        pageTitle: 'Add product',
+        path: '/admin/add-product',
+        productCSS: true,
+        formsCSS: true,
+        activeAddProduct: true
+    })
+    // res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
 })
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-    console.log(req.body);
+    products.push({ title: req.body.title });
     res.redirect('/');
 })
 
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
